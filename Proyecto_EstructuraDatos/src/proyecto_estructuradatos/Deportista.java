64f350b80e0da213c1f29c9a53a34b1e7b4cff12
Alerex12 <alejamunozvindas@hotmail.com>
2024-11-26 14:56:43 -0600

    Ingreso de clase deportista y creacion de la clase que une todos los subcatalogos

---
+++ Proyecto_EstructuraDatos/src/proyecto_estructuradatos/Deportista.java
@@ -0,0 +1,106 @@
+
+package proyecto_estructuradatos;
+
+public class Deportista {
+    
+    private String nombreUsuario;
+    private String identificacion;
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private Estado estado;
+
+    public Deportista() {
+        this.nombreUsuario = " ";
+        this.identificacion = " ";
+        this.nombre = nombre;
+        this.apellido = " ";
+        this.ciudad = " ";
+        this.direccion = " ";
+        this.telefono =" ";
+        this.correo = " ";
+        this.estado = Estado.ACTIVO;
+    }
+    
+    
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
+    
+}
---
+++ Proyecto_EstructuraDatos/src/proyecto_estructuradatos/Funcion_Subcatalogos.java
@@ -0,0 +1,6 @@
+
+package proyecto_estructuradatos;
+
+public class Funcion_Subcatalogos {
+    
+}
