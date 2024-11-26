4c728d85e324e7c069dddc0ed0bfcb239711ddc9
Alerex12 <alejamunozvindas@hotmail.com>
2024-11-26 16:36:01 -0600

    Agrego el nodo deportista y comentario sobre la clase funcion

---
+++ Proyecto_EstructuraDatos/src/proyecto_estructuradatos/NodoDeportista.java
@@ -0,0 +1,43 @@
+
+package proyecto_estructuradatos;
+
+public class NodoDeportista {
+    private Deportista elemento;
+    private NodoDeportista siguiente;
+    private NodoDeportista anterior;
+
+    public NodoDeportista() {
+        this.siguiente = null;
+        this.anterior = null;
+    }
+
+    public Deportista getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Deportista elemento) {
+        this.elemento = elemento;
+    }
+
+    public NodoDeportista getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoDeportista siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public NodoDeportista getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoDeportista anterior) {
+        this.anterior = anterior;
+    }
+    
+    
+    
+            
+    
+    
+}
